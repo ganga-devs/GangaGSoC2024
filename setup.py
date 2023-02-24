c645e99d6f64cbec1cbd2344f53df8424cc60930
Ulrik Egede <ulrik.egede@monash.edu>
2023-02-24 18:19:26 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,10 +19,9 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3.0',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
