c8c77474475e34c0b8ce449f8189170a4279bfb6
Ulrik Egede <ulrik.egede@monash.edu>
2023-03-09 10:56:06 +1100

    Added test example that demonstrates sleep_until_completed.

---
+++ test/test_ArgSplitter.py
@@ -0,0 +1,28 @@
+from ganga.GangaCore.testlib.GangaUnitTest import GangaUnitTest
+
+
+class TestArgSplitter(GangaUnitTest):
+    def testArgSplitter(self):
+        from GangaCore.GPI import Job, ArgSplitter
+        from ganga.GangaTest.Framework.utils import sleep_until_completed, file_contains
+
+        j = Job()
+        j.splitter = ArgSplitter(args=[['1'], ['2'], ['3']])
+
+        self.assertTrue(not j.subjobs)
+        self.assertEqual(len(j.subjobs), 0)
+
+        j.submit()
+
+        self.assertEqual(len(j.subjobs), 3)
+
+        for s in j.subjobs:
+            self.assertEqual(s.master, j)
+            self.assertIn(s.status, ['submitted', 'running', 'completed'])
+
+        self.assertTrue(sleep_until_completed(j, 60), 'Timeout on completing job')
+
+        for i, s in zip(range(len(j.subjobs)), j.subjobs):
+            self.assertEqual(s.status, 'completed')
+            self.assertTrue(file_contains(s.outputdir + '/stdout', '%d' % (i + 1)))
+
